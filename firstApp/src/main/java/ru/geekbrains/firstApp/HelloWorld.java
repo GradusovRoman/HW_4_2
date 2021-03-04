8bb04f219d76eadad33b3c0b9c02d21a6d06b6c3
GradusovRoman <monomari86@gmail.com>
2021-03-04 22:44:21 +0500

    HW_4_2

---
+++ firstApp/src/main/java/ru/geekbrains/firstApp/HelloWorld.java
@@ -0,0 +1,14 @@
+package ru.geekbrains.firstApp;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+
+	@RequestMapping("/hello")
+	public String sayHello() {
+		return "Hello World! Hello Geekbrains!";
+	}
+}
